b4e22ebcb567f4cfb7b238a7427856a43a46e746
Your Name <you@example.com>
2017-05-02 21:25:19 +0300

    style movies list

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -10,7 +10,6 @@ import {Http} from "@angular/http";
 })
 
 export class MoviesListComponent implements OnInit {
-
   movies;
 
   constructor(
@@ -20,28 +19,13 @@ export class MoviesListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e442ad82538a59f2029879dbb2ee3ece",
-      "method": "GET",
-      "headers": {},
-      "data": "{}"
-    }
+    this.titleService.setTitle(this.route.snapshot.data['title']);
 
-    this.titleService.setTitle( this.route.snapshot.data['title'] );
-
-
-
-    this.http.get(settings.url)
+    this.http.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e442ad82538a59f2029879dbb2ee3ece')
       .map(res => res.json())
       .subscribe(
-        data => {this.movies = data.results;
-
-          console.log(this.movies)
-        },
-        err => console.log(err),
-        () => console.log('Random Quote Complete')
+        data => this.movies = data.results,
+        err => console.log(err)
       );
 
 
