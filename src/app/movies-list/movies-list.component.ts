f6e7706d6326f9acda9ddc8c6a78625bd3e2f598
Your Name <you@example.com>
2017-05-10 14:35:04 +0300

    move url to var

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -4,12 +4,10 @@ import {ActivatedRoute} from "@angular/router";
 import {Http} from "@angular/http";
 
 @Component({
-  selector: 'app-movies-list',
+  selector: 'movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-
-
 
 export class MoviesListComponent implements OnInit {
   movies: Movie[];
@@ -21,14 +19,15 @@ export class MoviesListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    let url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e442ad82538a59f2029879dbb2ee3ece';
+
     this.titleService.setTitle(this.route.snapshot.data['title']);
 
-    this.http.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e442ad82538a59f2029879dbb2ee3ece')
-      .map(res => res.json())
-      .subscribe(
-        data => this.movies = data.results,
-        err => console.log(err)
-      );
+    this.http.get(url).subscribe(
+      data => this.movies = data.json().results,
+      err => console.log(err)
+    );
+
   }
 
 }
